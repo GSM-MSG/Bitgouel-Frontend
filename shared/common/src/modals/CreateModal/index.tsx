51b9da81a8ee464f0be3ef52081436273c81053d
TaerangLee <s22051@gsm.hs.kr>
2023-12-03 21:48:47 +0900

    Update closeModal alert

--- shared/common/src/modals/CreateModal/index.tsx
+++ shared/common/src/modals/CreateModal/index.tsx
@@ -29,7 +29,6 @@ const CreateModal = ({
             onClick={() => {
               onCreate()
               router.push('/main/lecture')
-              closeModal()
             }}
           >
             {createText}
