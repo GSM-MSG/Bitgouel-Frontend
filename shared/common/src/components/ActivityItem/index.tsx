f5b53831f15df5112010212e4b77d8e1d6aadeb8
TaerangLee <s22051@gsm.hs.kr>
2024-03-05 17:22:11 +0900

    Update style change, router destructurize

--- shared/common/src/components/ActivityItem/index.tsx
+++ shared/common/src/components/ActivityItem/index.tsx
@@ -7,11 +7,13 @@ import { lectureStatusToKor } from '../../constants'
 import * as S from './style'
 
 const ActivityItem = ({ item }: ActivityItemProps) => {
-  const router = useRouter()
+  const { push } = useRouter()
+
+  console.log(item)
 
   return (
     <S.ActivityItemWrapper
-      onClick={() => router.push('/main/club/student/activity/detail')}
+      onClick={() => push('/main/club/student/activity/detail')}
     >
       <div>
         <div>
@@ -31,18 +33,9 @@ const ActivityItem = ({ item }: ActivityItemProps) => {
           )}일 ${item.activityDate.slice(11, 16)}`}</S.Date>
         </div>
         <div>
-          <S.Uploader>{item.userName}</S.Uploader>
+          <S.Uploader>{item.username}</S.Uploader>
         </div>
       </div>
-      <S.StatusContainer>
-        <S.ApproveStatus
-          approveColor={match(item.approveStatus)
-            .with('APPROVED', () => true)
-            .otherwise(() => false)}
-        >
-          {lectureStatusToKor[item.approveStatus]}
-        </S.ApproveStatus>
-      </S.StatusContainer>
     </S.ActivityItemWrapper>
   )
 }
