15b1c07be00e388c48609243a36b9d594b3b535d
TaerangLee <s22051@gsm.hs.kr>
2024-03-10 16:00:34 +0900

    Update activityCreatPage peristalsis

--- packages/inside/src/PageContainer/Club/ActivityCreatePage/index.tsx
+++ packages/inside/src/PageContainer/Club/ActivityCreatePage/index.tsx
@@ -3,11 +3,25 @@
 import * as S from './style'
 import { useState } from 'react'
 import { Bg2 } from '@bitgouel/common'
-import { Chevron } from '@bitgouel/common'
+import { ApproveModal, Chevron, CreateModal, useModal } from '@bitgouel/common'
+import { usePostActivityInformation } from '@bitgouel/api'
 import { SelectCalendarTimeModal, SelectScoreModal } from '@bitgouel/common'
+import { toast } from 'react-toastify'
+import { useRouter } from 'next/navigation'
+import { StudentIdProps } from '@bitgouel/types'
 
-const ActivityCreatePage = () => {
-  const MAXLENGTH: number = 1000 as const
+interface StudentProps {
+  studentIdProps: StudentIdProps
+}
+
+const ActivityCreatePage: React.FC<StudentProps> = ({ studentIdProps }) => {
+  const { studentId, clubId } = studentIdProps
+
+  const MAXTITLELENGTH: number = 100 as const
+  const MAXCONTENTLENGTH: number = 1000 as const
+
+  const { openModal, closeModal } = useModal()
+  const { push } = useRouter()
 
   const [isActivityDate, setIsActivityDate] = useState<boolean>(false)
   const [activityDate, setActivityDate] = useState<Date>(new Date())
@@ -17,14 +31,52 @@ const ActivityCreatePage = () => {
   const [isScore, setIsScore] = useState<boolean>(false)
   const [scoreText, setScoreText] = useState<string>('학점 선택')
 
+  const [title, setTitle] = useState<string>('')
+  const [content, setContent] = useState<string>('')
+
   const openSelectModal = (type: string) => {
     if (type === '학점 선택') {
       setIsActivityDate(false)
       setIsScore((prev) => !prev)
-    } else {
+    } else if (type === '활동 날짜 선택') {
       setIsActivityDate((prev) => !prev)
       setIsScore(false)
     }
+  }
+
+  const { mutate } = usePostActivityInformation()
+
+  const onCreate = () => {
+    console.log('title' + title)
+    console.log('content' + content)
+    console.log('activityDate' + activityDate)
+    console.log('score' + scoreText)
+    console.log(
+      `${activityDate.getFullYear()}-${(activityDate.getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}-${activityDate
+        .getDate()
+        .toString()
+        .padStart(2, '0')}`
+    )
+
+    mutate({
+      content: content,
+      title: title,
+      credit: +scoreText.slice(0, 1),
+      activityDate: `${activityDate.getFullYear()}-${(
+        activityDate.getMonth() + 1
+      )
+        .toString()
+        .padStart(2, '0')}-${activityDate
+        .getDate()
+        .toString()
+        .padStart(2, '0')}`,
+    })
+
+    console.log(mutate)
+    // closeModal()
+    push(`/main/club/${clubId}/student/${studentId}/activity`)
   }
 
   return (
@@ -37,10 +89,17 @@ const ActivityCreatePage = () => {
       </S.SlideBg>
       <S.DocumentInputContainer>
         <S.DocumentInput>
-          <S.InputTitle placeholder='활동 제목(100자 이내)' maxLength={100} />
+          <S.InputTitle
+            placeholder='활동 제목(100자 이내)'
+            maxLength={MAXTITLELENGTH}
+            onChange={(e) => {
+              setTitle(e.target.value)
+            }}
+          />
           <S.InputMainText
-            maxLength={MAXLENGTH}
+            maxLength={MAXCONTENTLENGTH}
             placeholder='본문 입력 (1000자 이내)'
+            onChange={(e) => setContent(e.target.value)}
           />
           <S.ActivitySetting>
             <S.SettingTitle>활동 세부 설정</S.SettingTitle>
@@ -54,7 +113,9 @@ const ActivityCreatePage = () => {
                       setText={setActivityDateText}
                     />
                   )}
-                  <S.SettingDateBox onClick={() => openSelectModal('')}>
+                  <S.SettingDateBox
+                    onClick={() => openSelectModal('활동 날짜 선택')}
+                  >
                     <Chevron />
                     <S.SettingButton>{activityDateText}</S.SettingButton>
                   </S.SettingDateBox>
@@ -80,7 +141,26 @@ const ActivityCreatePage = () => {
             </S.SettingSelectionContainer>
           </S.ActivitySetting>
           <S.ButtonContainer>
-            <S.CreateButton>활동 신청하기</S.CreateButton>
+            <S.CreateButton
+              onClick={() =>
+                openModal(
+                  <CreateModal
+                    question='활동을 추가하시겠습니까?'
+                    title={title}
+                    onCreate={onCreate}
+                    createText='추가하기'
+                  />
+                )
+              }
+              isAble={
+                title !== '' &&
+                content !== '' &&
+                activityDateText !== '활동 날짜 선택' &&
+                scoreText !== '학점 선택'
+              }
+            >
+              활동 신청하기
+            </S.CreateButton>
           </S.ButtonContainer>
         </S.DocumentInput>
       </S.DocumentInputContainer>
